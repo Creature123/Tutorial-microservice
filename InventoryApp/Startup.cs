c42de5e42db2b42a8f32f1fe1898183cb5ce46c2
Sourav Das <sourav.besu.it.001@gmail.com>
2022-10-28 21:27:56 +0530

    Swagger Added
    Added Swagger Documentation Functionality

--- InventoryApp/Startup.cs
+++ InventoryApp/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace InventoryApp
 {
@@ -26,6 +27,17 @@ namespace InventoryApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen( c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "V1",
+                    Title = "InventoryManagementApp",
+                    Description = "A MicroserviceBased Architectureð"
+
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +46,11 @@ namespace InventoryApp
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "InventoryManagementApp");
+                });
             }
 
             app.UseHttpsRedirection();
