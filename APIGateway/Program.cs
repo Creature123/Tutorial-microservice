9ebb6a9043615cc7afd833c65df36a252899382c
Sourav Das <sourav.besu.it.001@gmail.com>
2022-10-29 19:59:21 +0530

    latest code checking
    latest code

--- APIGateway/Program.cs
+++ APIGateway/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+//using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace APIGateway
 {
@@ -13,6 +15,12 @@ namespace APIGateway
     {
         public static void Main(string[] args)
         {
+            string envName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: true)
+                .AddJsonFile($"appsettings.{envName}.json", optional: true).AddCommandLine(args).Build();
+
+            Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(config).CreateLogger();
+
             CreateHostBuilder(args).Build().Run();
         }
 
